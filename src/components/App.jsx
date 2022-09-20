3ce00c5225897f63a0cb74d27f36082874bccb18
Raphael Hazout <raphaelhazout@gmail.com>
2022-09-20 11:06:15 +0300

    Latest Version

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [Notes,setNotes] = useState([{title:"New Note", content:"Write Your Note Here!"}]);
+  function addNote(note){
+    setNotes(prevNotes => {return [...prevNotes,
+      note]});
+  }
+
+  function deleteNote(id){
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem,index)=>{
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onAdd={addNote}/>
+      { Notes.map((noteItem,index) => {
+        return <Note key={index} id={index} title={noteItem.title} content={noteItem.content} onDelete={deleteNote}/>
+      })}
       <Footer />
     </div>
   );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import Note from "./Note";
+import MapsUgcRoundedIcon from '@mui/icons-material/MapsUgcRounded';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
 
-function CreateArea() {
+
+function CreateArea(props) {
+  const [isExpanded,setExpanded] = useState(false);
+
+  const [note,setNote] = useState({
+    title:"",
+    content:""
+  });
+  
+  function handleChange(event) {
+    const {name,value} = event.target;
+
+    setNote(prevNote => {
+      return {
+      ...prevNote,
+      [name]: value
+      };
+    });
+  }
+
+  function submitNote(event){
+    props.onAdd(note);
+    setNote({  title:"",
+    content:""})
+    event.preventDefault();
+  }
+  function handleClick(){
+    setExpanded(true);
+  }
+
+
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+      <form className="create-note">
+      {isExpanded && <input onChange={handleChange} name="title" value={note.title} placeholder="Title" />}
+        <textarea onClick={handleClick} onChange={handleChange} name="content" placeholder="Take a note..." rows={isExpanded?"3":"1"} value={note.content} />
+        <Zoom in={isExpanded}>
+          <Fab onClick={submitNote}><MapsUgcRoundedIcon /></Fab>
+        </Zoom>
       </form>
     </div>
   );
